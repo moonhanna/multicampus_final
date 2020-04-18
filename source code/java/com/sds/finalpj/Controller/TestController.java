30933b346edba93d3d13b5e289331fb801a25b98
moonhanna <mhn97@naver.com>
2020-04-18 23:04:56 +0900

    update test.jsp

---
+++ source code/java/com/sds/finalpj/Controller/TestController.java
@@ -0,0 +1,22 @@
+package com.sds.finalpj.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value = "/test", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("center", "test");
+		mv.setViewName("index");
+
+		return mv;
+
+	}
+
+}
